3f79feef5f875c7d614bb14230adadb4900eae34
ahmed ragab <ar1755@fayoum.edu.eg>
2023-10-11 03:20:30 +0300

    addint provider for change language

--- lib/shared/controller/language_themeing_provider.dart
+++ lib/shared/controller/language_themeing_provider.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-class AppProvider extends ChangeNotifier{
+class AppProvider extends ChangeNotifier {
+  String languageCode = 'en';
 
-
-
+  changeLanguage(String language) {
+    languageCode = language;
+    notifyListeners();
+  }
 }
--- lib/shared/widgets/themeing_bottom_sheet.dart
+++ lib/shared/widgets/themeing_bottom_sheet.dart
@@ -5,6 +5,37 @@ class ThemeingBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Light'),
+                Icon(Icons.done),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Dark'),
+                Icon(Icons.done),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
